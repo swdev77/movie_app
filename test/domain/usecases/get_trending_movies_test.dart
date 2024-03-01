797fb838a5b7986ad7d36f9cc3b5e0b787461a50
swdev77 <swdev77@gmail.com>
2024-03-01 16:50:23 +0500

    get trending movies usecase

--- test/domain/usecases/get_trending_movies_test.dart
+++ test/domain/usecases/get_trending_movies_test.dart
@@ -1,17 +1,37 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:movie_app/domain/entities/movie.dart';
 import 'package:movie_app/domain/repositories/movie_repository.dart';
 import 'package:movie_app/domain/usecases/get_trending_movies.dart';
 
 class MockMovieRepository extends Mock implements MovieRepository {}
 
 void main() {
-  late GetTrendingMovies getTrendingMovies;
+  late GetTrendingMovies usecases;
   late MockMovieRepository mockMovieRepository;
 
   setUp(() {
     mockMovieRepository = MockMovieRepository();
-    getTrendingMovies = GetTrendingMovies(mockMovieRepository);
+    usecases = GetTrendingMovies(mockMovieRepository);
+  });
+
+  final moviesList = [
+    Movie(),
+    Movie(),
+  ];
+
+  test('should get trending movies from the repository', () async {
+    // arrange
+    when(() => mockMovieRepository.getTrendingMovies())
+        .thenAnswer((_) async => moviesList);
+
+    // act
+    final result = await usecases.execute();
+
+    // assert
+    expect(result, moviesList);
+    verify(() => mockMovieRepository.getTrendingMovies()).called(1);
+    verifyNoMoreInteractions(mockMovieRepository);
   });
 
   test(
@@ -22,7 +42,7 @@ void main() {
       ).thenAnswer(
         (_) => Future.value([]),
       );
-      await getTrendingMovies.execute();
+      await usecases.execute();
       verify(() => mockMovieRepository.getTrendingMovies()).called(1);
     },
   );
