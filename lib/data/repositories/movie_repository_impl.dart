53d34597a6433dd815a6cc3c7b8e18b1b9531545
swdev77 <swdev77@gmail.com>
2024-03-12 11:36:53 +0500

    move repository impl completed

--- lib/data/repositories/movie_repository_impl.dart
+++ lib/data/repositories/movie_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:movie_app/data/datasources/movie_remote_data_source.dart';
+import 'package:movie_app/data/models/movie_model.dart';
 import 'package:movie_app/domain/entities/movie.dart';
 
 import '../../domain/repositories/movie_repository.dart';
@@ -9,27 +10,34 @@ class MovieRepositoryImpl implements MovieRepository {
   MovieRepositoryImpl({required this.movieRemoteDataSource});
 
   @override
-  Future<List<Movie>> getPopularMovies() {
-    // TODO: implement getPopularMovies
-    throw UnimplementedError();
+  Future<List<Movie>> getPopularMovies() async {
+    final movieModels = await movieRemoteDataSource.getPopularMovies();
+    return _mapMovieModels(movieModels);
   }
+
 
   @override
   Future<List<Movie>> getTrendingMovies() async {
-    final movieModels = await movieRemoteDataSource
-      .getTrendingMovies();
-    final movies = movieModels.map((movieModel) => Movie(
-      id: movieModel.id,
-      title: movieModel.title,
-      overview: movieModel.overview,
-      posterPath: movieModel.posterPath,
-    ));
-    return movies.toList();
+    final movieModels = await movieRemoteDataSource.getTrendingMovies();
+    return _mapMovieModels(movieModels);
   }
 
   @override
-  Future<List<Movie>> searchMovies(String query) {
-    // TODO: implement searchMovies
-    throw UnimplementedError();
+  Future<List<Movie>> searchMovies(String query) async {
+    final movieModels = await movieRemoteDataSource.searchMovies(query); 
+    return _mapMovieModels(movieModels);
+  }
+  
+  List<Movie> _mapMovieModels(List<MovieModel> movieModels) {
+    return movieModels
+        .map(
+          (movieModel) => Movie(
+            id: movieModel.id,
+            title: movieModel.title,
+            overview: movieModel.overview,
+            posterPath: movieModel.posterPath,
+          ),
+        )
+        .toList();
   }
 }
--- test/data/repositories/move_repository_impl_test.dart
+++ test/data/repositories/move_repository_impl_test.dart
@@ -14,7 +14,7 @@ void main() {
   final movieRepository =
       MovieRepositoryImpl(movieRemoteDataSource: mockRemoteDataSource);
 
-  final List<MovieModel> movieModels = MovieModels.list ;
+  final List<MovieModel> movieModels = MovieModels.list;
   final List<Movie> movies = Movies.list;
 
   test('should get trending movies', () async {
@@ -28,5 +28,32 @@ void main() {
     // assert
     expect(result, movies);
     verify(() => mockRemoteDataSource.getTrendingMovies()).called(1);
+  });
+
+  test('should get popular movies', () async {
+    // arrange
+    when(() => mockRemoteDataSource.getPopularMovies())
+        .thenAnswer((_) async => movieModels);
+
+    // act
+    final result = await movieRepository.getPopularMovies();
+
+    // assert
+    expect(result, movies);
+    verify(() => mockRemoteDataSource.getPopularMovies()).called(1);
+  });
+
+  test('should search movies', () async {
+    // arrange
+    const query = 'spiderman';
+    when(() => mockRemoteDataSource.searchMovies(query))
+        .thenAnswer((_) async => movieModels);
+
+    // act
+    final result = await movieRepository.searchMovies(query);
+
+    // assert
+    expect(result, movies);
+    verify(() => mockRemoteDataSource.searchMovies(query)).called(1);
   });
 }
